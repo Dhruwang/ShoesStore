fc400291fd3b69422b0a24a941082c0070cac77f
Dhruwang <dhruwangjariwala18@gmail.com>
2022-11-26 14:02:22 +0530

    completed about us

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
         <div className="row" style={{ backgroundImage: `url(${cover})`, height: "100vh", backgroundSize: 'cover'}}>
-            <div className='homeText my-auto col-sm-12 col-md-6 mx-4 text-left'>
+            <div className='homeText my-auto col-sm-12 col-md-6 mx-4 text-left reveal'>
                 <h1 style={{fontSize:"6vw"} }className='text-light text-left'>TheShoeStore</h1>
                 <p className='text-light'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae eveniet ullam perferendis eos, nobis corrupti similique fuga ipsa minus at eius ipsam expedita quam aliquam perspiciatis voluptate qui dolore soluta.</p>
                 <button type="button" class="btn btn-light rounded px-4">Order Now</button>
